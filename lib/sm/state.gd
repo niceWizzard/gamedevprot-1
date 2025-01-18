de01829f1d0dcb8460e79cd64139f4b0b6898c47
RIchard Manansala <richard232manansala@gmail.com>
2025-01-18 11:29:53 +0800

    added some sm/state boilerplate

---
+++ lib/sm/state.gd
@@ -0,0 +1,28 @@
+class_name RState
+extends Node
+
+var sm_owner : Node
+
+func _ready() -> void:
+    set_physics_process(false)
+    set_process(false)
+
+func setup(sm_owner : Node) -> void:
+    self.sm_owner = sm_owner
+
+
+func on_enter() -> void:
+    pass
+
+func on_exit() -> void:
+    pass
+
+func on_process(delta: float) -> RState:
+    return null
+
+
+func on_physics_process(delta:  float) -> RState:
+    return null
+
+
+
---
+++ lib/sm/state_machine.gd
@@ -0,0 +1,44 @@
+class_name StateMachine
+extends Node
+
+@export var initial_state : RState
+
+var current_state : RState = null
+
+
+func _ready() -> void:
+    var child_states :Array[RState]= get_child_states() 
+    assert(child_states.size() > 0, "There is no child states in the state machine!")
+
+    for state in child_states:
+        state.setup(self.get_parent())
+
+    if initial_state == null:
+        initial_state = child_states[0]
+
+    set_state(current_state)
+
+
+func _process(delta: float) -> void:
+    var new_state = current_state.on_process(delta)
+    if new_state != null:
+        set_state(new_state)
+
+func _physics_process(delta: float) -> void:
+    var new_state = current_state.on_physics_process(delta)
+    if new_state != null:
+        set_state(new_state)
+
+func set_state(s : RState) -> void:
+    if current_state != null:
+        current_state.on_exit()
+    current_state = s
+    s.on_enter()
+
+func get_child_states() -> Array[RState]:
+    var array : Array[RState] = []
+    for child : Node in get_children():
+        if child is RState:
+            array.append(child)
+    return array
+
