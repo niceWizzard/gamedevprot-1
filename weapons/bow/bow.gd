4f54ff8118d9627693cbbd4ece2d232ee7cd1d34
RIchard Manansala <richard232manansala@gmail.com>
2025-01-23 21:12:40 +0800

    Made bow owner be exported instead of using get_parent directly

--- weapons/bow/bow.gd
+++ weapons/bow/bow.gd
@@ -7,14 +7,19 @@ extends Sprite2D
 @export var arrow_spawn : Marker2D
 @export var arrow : PackedScene
 @export var arrow_placeholder_sprite : Sprite2D
+@export var bow_owner : Node2D
 
 var can_shoot := true
+
+func _ready() -> void:
+	if !bow_owner:
+		bow_owner = get_parent()
 
 func shoot(dir : Vector2) -> void:
 	if !can_shoot:
 		return
 	var arrow_obj = arrow.instantiate() as Projectile
-	arrow_obj.setup(dir, get_parent())
+	arrow_obj.setup(dir, bow_owner )
 	arrow_obj.global_position = arrow_spawn.global_position
 	get_tree().root.add_child(arrow_obj)
 	can_shoot = false
